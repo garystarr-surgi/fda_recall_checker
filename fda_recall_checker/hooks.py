440b30d3bf56451606a7dc206a3af8b60e667654
garystarr-surgi <gary.starr@surgishop.com>
2025-12-02 12:10:53 -0500

    Update hooks.py

--- fda_recall_checker/hooks.py
+++ fda_recall_checker/hooks.py
@@ -1,1 +1,15 @@
+app_version = "0.0.1"
+app_name = "fda_recall_checker"
+app_title = "FDA Recall Checker"
+app_publisher = "SurgiShop"
+app_description = "FDA Recall Checker"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "gary.starr@surgishop.com"
+app_license = "MIT"
 
+scheduler_events = {
+    "daily": [
+        "fda_recall_checker.fda_recall_checker.fetch_fda_recalls"
+    ]
+}
