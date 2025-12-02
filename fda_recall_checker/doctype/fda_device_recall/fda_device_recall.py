eacf57ecffb1d0cdf2f57b68b1f5434f5ff9830e
garystarr-surgi <gary.starr@surgishop.com>
2025-12-02 15:05:56 -0500

    Update fda_device_recall.py

--- fda_recall_checker/doctype/fda_device_recall/fda_device_recall.py
+++ fda_recall_checker/doctype/fda_device_recall/fda_device_recall.py
@@ -1,1 +1,27 @@
+# fda_device_recall.py
+
+# Copyright (c) 2025, Your Company and Contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+class FDADeviceRecall(Document):
+    """
+    DocType class for FDA Device Recall.
+
+    Fields:
+    - recall_number
+    - device_name
+    - manufacturer
+    - product_code
+    - recall_date
+    - reason
+    - status
+    - recall_firm
+    - code_info
+    """
+
+    # Optional: you can add helper methods here in the future
+    pass
 
