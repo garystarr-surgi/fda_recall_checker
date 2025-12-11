43c8349e629be0e8f067058a695eecd7d22c4523
garystarr-surgi <gary.starr@surgishop.com>
2025-12-11 13:17:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 from database import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///instance/fda_recalls.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///fda_recalls.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production')
 
